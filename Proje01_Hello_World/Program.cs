339f6c497a902503be945a714662912430ab7cfc
Nilyya <104194557+Nilyya@users.noreply.github.com>
2022-10-04 11:52:19 +0300

    3ekim

---
+++ Proje01_Hello_World/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Proje01_Hello_World
+{
+    internal class Program//burası programın ana scopenun başlangıç noktası
+    {
+        static void Main(string[] args)//burasıda ilk çalışacak olan METOT!!
+        {
+            Console.WriteLine("Merhaba, Dünya!");
+            Console.WriteLine("Benim adım Aylin!");
+        }
+    }
+}
