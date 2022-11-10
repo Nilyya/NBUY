007ee1b29d8a269cb997ab67ca22474ea347a315
Nilyya <104194557+Nilyya@users.noreply.github.com>
2022-11-10 12:10:16 +0300

    9.11

--- Week_06/DotNetCore-VeriTabanıİslemleri/Proje04_VeriErisimSınıfı/Product.cs
+++ Week_06/DotNetCore-VeriTabanıİslemleri/Proje04_VeriErisimSınıfı/Product.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Proje04_VeriErisimSınıfı
+namespace Proje04_VeriErisimSinifi
 {
     public class Product
     {
--- Week_06/DotNetCore-VeriTabanıİslemleri/Proje04_VeriErisimSınıfı/Program.cs
+++ Week_06/DotNetCore-VeriTabanıİslemleri/Proje04_VeriErisimSınıfı/Program.cs
@@ -1,79 +1,117 @@
 ﻿using System.Data.SqlClient;
 
-namespace Proje04_VeriErisimSınıfı;
+namespace Proje04_VeriErisimSinifi;
 class Program
 {
     static void Main(string[] args)
     {
-        // List<Product> products=GetAllProducts();
-        // foreach (var product in products)
-        // {
-        //     Console.WriteLine($"Id:{product.Id}, Name:{product.Name}, Price:{product.Price}, Stock:{product.Stock}");
-        // }
-        List<Customer> customers=GetCustomers();
+        int secim;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("1-Product List");
+            Console.WriteLine("2-Customer List");
+            Console.WriteLine("0-Exit");
+            Console.Write("Lütfen seçiminizi giriniz: ");
+            secim = int.Parse(Console.ReadLine());
+            if (secim == 1)
+            {
+                ProductList();
+            }
+            else if (secim == 2)
+            {
+                CustomerList();
+            }
+            else if (secim != 0)
+            {
+                Console.WriteLine("Lütfen geçerli bir seçim yapınız!");
+            }
+            Console.ReadLine();
+        } while (secim != 0);
+    }
+    static void ProductList()
+    {
+        List<Product> products = GetAllProducts();
+        foreach (var product in products)
+        {
+            Console.WriteLine($"Id: {product.Id}, Name: {product.Name}, Price: {product.Price}, Stock: {product.Stock}");
+        }
+    }
+    static void CustomerList()
+    {
+        List<Customer> customers = GetAllCustomers();
         foreach (var customer in customers)
         {
-            
+            Console.WriteLine($"Id: {customer.CustomerID}, Company: {customer.CompanyName}, City: {customer.City}, Country: {customer.Country}");
         }
     }
-    static List<Customer> GetCustomers()
+    static List<Customer> GetAllCustomers()
     {
-        List<Customer> customers= new List<Customer>();
-        using(var connection=GetSqlConnection())
-        {
-            try
-            {
-            connection.Open();
-            string queryString="SELECT";
-              SqlCommand sqlCommand=new SqlCommand(queryString, connection);
-                SqlDataReader sqlDataReader=sqlCommand.ExecuteReader();
-                while (sqlDataReader.Read())
-                {
-                    customers.Add(new Customer(){
-                        CustomerID= int.Parse(sqlDataReader[0].ToString()),
-                        CompanyName=sqlDataReader[1].ToString(),
-                       City=sqlDataReader[2].ToString(),
-                        Country=sqlDataReader[3].ToString()
-                    });
-                }
-                sqlDataReader.Close();
-            }  
-      
-        }
-        return customers;
-    }
-    static List<Product> GetAllProducts()
-    {
-        List<Product> products= new List<Product>();
-        using (var connection= GetSqlConnection())
+        List<Customer> customers = new List<Customer>();
+        using (var connection = GetSqlConnection())
         {
             try
             {
                 connection.Open();
-                string queryString="SELECT ProductId, ProductName, UnitPrice, UnitsInStock FROM Products";
-                SqlCommand sqlCommand=new SqlCommand(queryString, connection);
-                SqlDataReader sqlDataReader=sqlCommand.ExecuteReader();
+                string queryString = "SELECT CustomerID, CompanyName, City, Country FROM Customers";
+                SqlCommand sqlCommand = new SqlCommand(queryString, connection);
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
                 while (sqlDataReader.Read())
                 {
-                    products.Add(new Product(){
-                        Id=int.Parse(sqlDataReader[0].ToString()),
-                        Name=sqlDataReader[1].ToString(),
-                        Price=decimal.Parse(sqlDataReader[2].ToString()),
-                        Stock=int.Parse(sqlDataReader[3].ToString())
+                    customers.Add(new Customer()
+                    {
+                        CustomerID = int.Parse(sqlDataReader["CustomerID"].ToString()),
+                        CompanyName = sqlDataReader["CompanyName"].ToString(),
+                        City = sqlDataReader["City"].ToString(),
+                        Country = sqlDataReader["Country"].ToString()
                     });
                 }
                 sqlDataReader.Close();
             }
             catch (Exception)
             {
-                
-              Console.WriteLine("Bir sorun oluştu");
+                Console.WriteLine("Bir sorun oluştu");
             }
             finally
             {
                 connection.Close();
             }
         }
+        return customers;
+    }
+    static List<Product> GetAllProducts()
+    {
+        List<Product> products = new List<Product>();
+        using (var connection = GetSqlConnection())
+        {
+            try
+            {
+                connection.Open();
+                string queryString = "SELECT ProductId, ProductName, UnitPrice, UnitsInStock FROM Products";
+                SqlCommand sqlCommand = new SqlCommand(queryString, connection);
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                while (sqlDataReader.Read())
+                {
+                    products.Add(new Product()
+                    {
+                        Id = int.Parse(sqlDataReader[0].ToString()),
+                        Name = sqlDataReader[1].ToString(),
+                        Price = decimal.Parse(sqlDataReader[2].ToString()),
+                        Stock = int.Parse(sqlDataReader[3].ToString())
+                    });
+                }
+                sqlDataReader.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bir sorun oluştu!");
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
         return products;
     }
     static SqlConnection GetSqlConnection()
