9191a0d4f64739cd96c79ff48cef7bfcb8c6d91f
Nilyya <104194557+Nilyya@users.noreply.github.com>
2022-11-17 16:50:37 +0300

    17.11

---
+++ Week_07/Proje05_MVC_EfCore_CodeFirst - Kopya/Migrations/20221116135418_InitalDb.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proje05_MVC_EfCore_CodeFirst.Migrations
+{
+    public partial class InitalDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Desc = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Citys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Citys", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Desc = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Desc", "Name" },
+                values: new object[] { 1, "Phones", "Phone" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Desc", "Name" },
+                values: new object[] { 2, "Computers", "Computer" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Desc", "Name" },
+                values: new object[] { 3, "Electronics", "Electronic" });
+
+            migrationBuilder.InsertData(
+                table: "Citys",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 6, "Ankara" });
+
+            migrationBuilder.InsertData(
+                table: "Citys",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 34, "İstanbul" });
+
+            migrationBuilder.InsertData(
+                table: "Citys",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 35, "İzmir" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Desc", "Name", "Price" },
+                values: new object[] { 1, 1, "Güzel bir telefon", "IPhone13", 25000m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Desc", "Name", "Price" },
+                values: new object[] { 2, 2, "Güzel bir bilgisayar", "Dell", 15000m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Desc", "Name", "Price" },
+                values: new object[] { 3, 3, "Güzel bir sarj", "Powerbank", 5990m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Desc", "Name", "Price" },
+                values: new object[] { 4, 1, "Güzel bir telefon", "IPhone14", 35000m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Citys");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
