2ee8a9bd4d1237521f7618dd06e59ebcbb032062
Nilyya <ydmryln@gmail.com>
2022-11-18 21:19:56 +0300

    18.11

--- Week_07/Proje06_ModelBinding_Form/Controllers/HomeController.cs
+++ Week_07/Proje06_ModelBinding_Form/Controllers/HomeController.cs
@@ -6,54 +6,50 @@ namespace Proje06_ModelBinding_Form.Controllers;
 
 public class HomeController : Controller
 {
-
-
     public IActionResult Index()
     {
         return View();
     }
-    [HttpGet] //Gerek yok yazmayıncada aynı şeyi kabul ediyor
-    public IActionResult YasGrubu()
+    
+    public IActionResult YasGrubu(){
+        return View();
+    }
+    [HttpPost]
+    public IActionResult YasGrubu(int yas, string ad){
+        if(yas==0){
+            ViewBag.YasGrubu="Lütfen bir yaş bilgisi giriniz!";
+        }
+        else if (yas<18)
+        {
+            ViewBag.YasGrubu=$"{ad}, reşit değilsin!";
+        }else if (yas<40)
+        {
+            ViewBag.YasGrubu=$"{ad}, gençsin!";
+        }else if (yas<60)
+        {
+            ViewBag.YasGrubu=$"{ad}, gençlere taş çıkartırsın!";
+        }else {
+            ViewBag.YasGrubu=$"{ad}, hala emekli olmadın mı?";
+        }
+        return View();
+    }
+
+    public IActionResult CreateProduct()
     {
         return View();
     }
     [HttpPost]
-    public IActionResult YasGrubu(int yas, string ad)
-    {  
-        if (yas == 0)
-        {
-            ViewBag.YasGrubu = $"Lütfen yaş bilgisi giriniz.";
-        }
-        else if (yas < 18)
-        {
-            ViewBag.YasGrubu = $"{ad},Reşit Değilsiniz";
-        }
-        else if (yas < 40)
-        {
-            ViewBag.YasGrubu = $"{ad},Gençsiniz";
-        }
-        else if (yas < 60)
-        {
-            ViewBag.YasGrubu = $"{ad},Gençlere taş çıkarırsınız";
-        }
-        else
-        {
-            ViewBag.YasGrubu = $"{ad},Emekli olmadınız mı";
-        }
-        return View();
+    public IActionResult CreateProduct(Product product)
+    {
+        //Burada veri tabanına kayıt işlemi vb. yapılacak.
+        //Şimdilik biz gelen verilerin testini yapabilmek için tekrar sayfaya bastıralım.
+        // ViewBag.ResultHeader = $"{productName} adlı ürün eklendi.";
+        // ViewBag.ResultBody=$"Category: {productCategory}, Price: {productPrice}";
+        return View(product);
     }
-public IActionResult CreateProduct(Product product)
-{
-    return View(product);
-}
-public IActionResult CreateProduct()
-{
-    //Burada kayıt işlemi
-    //Şimdilik veri testi için
-    // ViewBag.ResultHeader=$"{productName} adlı ürün eklendi";
-    // ViewBag.ResultBody=$"Category:{productCategory},Price:{productPrice}";
-    return View();
-}
+    
+
+
     // public IActionResult VerileriAl(string txtAd, int txtYas)
     // {
     //     return View();
--- Week_07/Proje06_ModelBinding_Form/Models/ErrorViewModel.cs
+++ Week_07/Proje06_ModelBinding_Form/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Proje06_ModelBinding_Form.Models;
 
 public class ErrorViewModel
 {
-    public string RequestId { get; set; }
+    public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
